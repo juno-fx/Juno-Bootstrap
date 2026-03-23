1af9751450ccf05b4709fa1c1c54cfe0a9040b2e
aldmbmtl <alex.e.hatfield@gmail.com>
2026-03-23 16:54:41 -0400

    Fixing linter

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env bash
+
 curl -sL "$(curl -s https://api.github.com/repos/juno-fx/Juno-Bootstrap/releases/latest | grep browser_download_url | grep orion-install-helper | cut -d '"' -f 4)" | bash -
